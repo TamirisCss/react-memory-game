1479b1606c421a5ebcfb3fe1883fd0467559caef
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-05-31 19:57:05 +0200

    created event to get value from radioBox

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,16 +1,17 @@
+import { useState } from "react";
 import { CATEGORIES } from "../../constants";
+import RadioBox from "./RadioBox";
 
 const Settings = () => {
+  const [category, setCategory] = useState(CATEGORIES[1]);
+
   return (
     <div>
       <h2>Settings</h2>
       <h4>Category:</h4>
       <div>
         {CATEGORIES.map((item) => (
-          <div>
-            <input type="radio" key={item} name={item} id={item} value={item} />
-            <label htmlFor={item}>{item}</label>
-          </div>
+          <RadioBox key={item} item={item} category={category} onChange={e => setCategory(e.target.value)} />
         ))}
       </div>
     </div>
