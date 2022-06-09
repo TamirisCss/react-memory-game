ca5a798fc45b6974bf0a6de8664aac9d51747cda
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-06-09 14:40:44 +0200

    getting settingsOptions from App to get the images

--- src/hooks/getImages.js
+++ src/hooks/getImages.js
@@ -2,35 +2,37 @@ import { useEffect, useState } from "react";
 
 const getRandomPage = () => Math.round(Math.random() * (10 - 1) + 1);
 
-const GetImages = () => {
+const GetImages = (settingsOptions) => {
   const [images, setImages] = useState([]);
 
   const buildUrl = () => {
     let url = new URL("https://api.pexels.com/v1/search");
 
     url.search = new URLSearchParams({
-      query: "nature", //change to a varible
+      query: settingsOptions.selectCategory,
       orientation: "square",
       size: "small",
-      per_page: 2, //change to avarible
+      per_page: settingsOptions.selectCardsCounter / 2,
       page: getRandomPage(),
     });
     return url;
   };
 
   const fecthPics = () => {
-    fetch(buildUrl(), {
+    fetch(/*"https://cors-anywhere.herokuapp.com/" + */ buildUrl(), {
       headers: {
-        Authorization: process.env.PEXEL_API_KEY,
+        Authorization: process.env.REACT_APP_PEXEL_API_KEY,
       },
     })
       .then((data) => data.json())
-      .then((data) => setImages(data.photos));
+      .then((data) => setImages(data.photos))
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
+    if (!settingsOptions) return;
     fecthPics();
-  }, []);
+  }, [settingsOptions]);
 
   return images;
 };
