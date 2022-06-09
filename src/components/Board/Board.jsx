303084d7bc161fd11946bc621884779c35f8b42f
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-06-09 15:08:55 +0200

    created loading state

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GetImages from "./../../hooks/GetImages";
 
 const Board = ({ settingsOptions }) => {
+  const [loading, setLoading] = useState(true);
   const images = GetImages(settingsOptions);
-  console.log({ images });
-  return <div>Board</div>;
+
+  useEffect(() => {
+    if (images.length > 0) setLoading(false);
+  }, [images]);
+
+  return <div>{loading && <p>Loading...</p>}</div>;
 };
 
 export default Board;
