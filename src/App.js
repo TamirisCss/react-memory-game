0baafe57579268b89aac0b892c018c4a81b3da44
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-06-09 14:31:45 +0200

    created startGame function and passed it as a prop to settings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import './components/background/background.css';
-import Background from './components/background/Background';
+import "./components/background/background.css";
+import Background from "./components/background/Background";
 import GetImages from "./hooks/GetImages";
 import Settings from "./components/settings/Settings";
+import Board from "./components/Board/Board";
+import { useState } from "react";
 
 const App = () => {
-  const images = GetImages();
+  const [settingsOptions, setSettingsOptions] = useState(null);
 
-  console.log({ images })
+  const images = GetImages(settingsOptions);
+  console.log({ images });
+
+  const startGame = (options) => {
+    setSettingsOptions(options);
+  };
 
   return (
     <div className="background-content">
       <Background />
-      <h1 className='title'>Memory Game</h1>
-      <Settings />
+      <h1 className="title">Memory Game</h1>
+      <Settings startGame={startGame} />
+      {/* {images.length > 0 && <Cards />} */}
     </div>
   );
 };
